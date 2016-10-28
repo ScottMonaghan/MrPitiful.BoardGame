871e24f6b03cbd239c75f4dd8c54e96c2ecc82e9
Scott Monaghan <scott@milyli.com>
2016-10-28 17:24:21 -0500

    Trying to make a SUPER SIMPLE way to save objects to disk

---
+++ src/MrPitiful.BoardGame.Base/Entities/SerializedGameObject/SerializedGameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrPitiful.BoardGame.Base
+{
+    public class SerializedGameObject
+    {
+        public Guid Id { get; set; }
+        public Guid GameId { get; set; }
+        public String value { get; set; }
+    }
+}
