f4954671d78cb2077c8914cd273c0f24c9fb4272
Scott Monaghan <scott@smkkstudios.com>
2016-11-21 22:35:47 -0600

    Crated separate MrPitiful.BoardGame.Models project
    Data transfer objects!?  WE DON'T NEED NO STEEEENKING DATA TRANSFER
    OBJECTS!

---
+++ MrPitiful.BoardGame.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MrPitiful.BoardGame.Models")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32e4b09a-82a7-42cd-af81-22d6663f22d2")]
